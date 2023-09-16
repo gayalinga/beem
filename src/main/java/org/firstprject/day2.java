69624eb634caff4b45077fa57e3f2ca723fa2965
gayalinga <gayathrilinga21@gmail.com>
2023-09-16 00:18:00 -0700

    project structure

---
+++ src/main/java/org/firstprject/day2.java
@@ -0,0 +1,17 @@
+package org.firstprject;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+	
+		
+	
+
+@Test
+public  void test1() {
+	System.out.println("test1");
+	System.out.println("test2");
+	System.out.println("today beautiful day");
+
+}
+}
